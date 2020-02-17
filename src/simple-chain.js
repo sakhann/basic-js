093cc05b4d472f9fd7fa71b6290d6d689e529717
Sakhan Siradzhov <60882441+sakhann@users.noreply.github.com>
2020-02-17 14:49:21 +0300

    fix: update

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,28 @@
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.push('( '+ value +' )');
+    return this;
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (position < 1 || position > this.chain.length || !Number.isInteger(position)) {
+      this.chain = [];
+      throw new Error;
+    }
+    this.chain.splice(position-1, 1);
+    return this;
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let out = this.chain.join('~~');
+    this.chain = [];
+    return out;
   }
 };
 
