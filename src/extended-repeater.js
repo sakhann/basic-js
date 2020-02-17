f8139d00bb05d33db438e8d9c2102f22a22ac9fe
Sakhan Siradzhov <60882441+sakhann@users.noreply.github.com>
2020-02-17 14:47:34 +0300

    fix: update

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,21 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let output = '';
+  str = '' + str;
+  
+  if(options.addition === undefined) options.addition = '';
+  if(options.additionSeparator === undefined) options.additionSeparator = '|';
+  if(options.additionRepeatTimes === undefined) options.additionRepeatTimes = 1;
+  if(options.repeatTimes === undefined) options.repeatTimes = 1;
+  if(options.separator === undefined) options.separator = '+';
+
+  for (let i = 1; i <= options.repeatTimes; i++) {
+    output+= str;
+    for (let j = 1; j <= options.additionRepeatTimes; j++) {
+      output+= options.addition;
+      if(j < options.additionRepeatTimes) output+= options.additionSeparator;
+    }
+    if(i < options.repeatTimes) output+= options.separator;
+  }
+  return output;
 };
   
